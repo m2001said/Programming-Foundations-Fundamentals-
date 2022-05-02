2ffc35c9a487139b9ec881c8462355f7a888d11d
m2001said <m277712@gmail.com>
2022-05-02 02:47:46 +0200

    the first commit

---
+++ the final challenge .py
@@ -0,0 +1,11 @@
+
+#make the function (which called creating the function)
+def favorite_city(name):
+    print("One of my favorite cities is", name)
+
+#calling the function because if you don't call it , it willnot appear
+#It's required to call it 3 times
+favorite_city("Santa Barbara, California")
+favorite_city("Asheville, North Carolina")
+favorite_city("Amsterdam, The Netherlands")
+
---
+++ the second challenge.py
@@ -0,0 +1,9 @@
+#the first challenge in this course
+print("can you gess ? ")
+food = input("what is my favorite food ?")
+if food == "meat":
+   print("yes it is ok")
+else: print("no")
+print("thanks for playing")
+
+
